eaff7496da1c1f6829cf829c5506f37ef4314499
Steven Rojano <stevenrojano@Stevens-MBP.lan>
2023-01-16 17:13:34 -0700

    created a custom expectations, will look into defining some more complex logic using the different decorators available in great expectations.

---
+++ great_expectations/plugins/expectations/expect_column_max_to_be_between_custom.py
@@ -0,0 +1,197 @@
+"""
+This is a template for creating custom ColumnExpectations.
+For detailed instructions on how to use it, please see:
+    https://docs.greatexpectations.io/docs/guides/expectations/creating_custom_expectations/how_to_create_custom_column_aggregate_expectations
+"""
+
+from doctest import Example
+from typing import Dict, Optional
+
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
+from great_expectations.exceptions import InvalidExpectationConfigurationError
+from great_expectations.execution_engine import (
+    ExecutionEngine,
+    PandasExecutionEngine,
+    SparkDFExecutionEngine,
+    SqlAlchemyExecutionEngine,
+)
+from great_expectations.expectations.expectation import ColumnExpectation
+from great_expectations.expectations.metrics import (
+    ColumnAggregateMetricProvider,
+    column_aggregate_partial,
+    column_aggregate_value,
+)
+from great_expectations.expectations.metrics.import_manager import F
+
+# This class defines a Metric to support your Expectation.
+# For most ColumnExpectations, the main business logic for calculation will live in this class.
+class ColumnCustomMax(ColumnAggregateMetricProvider):
+
+    # This is the id string that will be used to reference your Metric.
+    metric_name = "column.custom_max"
+
+    # This method implements the core logic for the PandasExecutionEngine
+    @column_aggregate_value(engine=PandasExecutionEngine)
+    def _pandas(cls, column, **kwargs):
+        """Pandas Max Implementation"""
+        return column.max()
+
+    # This method defines the business logic for evaluating your Metric when using a SqlAlchemyExecutionEngine
+    # @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
+    # def _sqlalchemy(cls, column, _dialect, **kwargs):
+    #     raise NotImplementedError
+    #
+    # This method defines the business logic for evaluating your Metric when using a SparkDFExecutionEngine
+    @column_aggregate_partial(engine=SparkDFExecutionEngine)
+    def _spark(cls, column, _table, _column_name, **kwargs):
+        return F.max(column)
+
+
+# This class defines the Expectation itself
+class ExpectColumnMaxToBeBetweenCustom(ColumnExpectation):
+    """TODO: We are expecting all sales to fall max range of 1,000,000 and 10,000,000 after performing aggregation"""
+
+    # These examples will be shown in the public gallery.
+    # They will also be executed as unit tests for your Expectation.
+    # an example is similar to dummy data that you create in a pytest. We provide this information as a test
+    # so that users can see what our customer exceptions are doing.
+    examples = [
+        {
+            "data": {"x": [1000000, 10000000, 8000000, 4000000, 5000000], "y": [500, 800, 1000, 2000, 9000]},
+            "tests": [
+                {
+                    "title": "basic_positive_test",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "x",
+                        "min_value": 1000000,
+                        "strict_min": True,
+                        "max_value": 10000000,
+                        "strict_max": False,
+                    },
+                    "out": {"success": True},
+                },
+                {
+                    "title": "basic_negative_test",
+                    "exact_match_out": False,
+                    "include_in_gallery": True,
+                    "in": {
+                        "column": "y",
+                        "min_value": 500,
+                        "strict_min": False,
+                        "max_value": 2000,
+                        "strict_max": True,
+                    },
+                    "out": {"success": False},
+                },
+            ],
+            "test_backends": [
+                {
+                    "backend": "pandas",
+                    "dialects": None,
+                },
+                {
+                    "backend": "sqlalchemy",
+                    "dialects": ["sqlite", "postgresql"],
+                },
+                {
+                    "backend": "spark",
+                    "dialects": None,
+                },
+            ],
+        }
+    ]
+
+    # This is a tuple consisting of all Metrics necessary to evaluate the Expectation.
+    metric_dependencies = ("column.custom_max",)
+
+    # This a tuple of parameter names that can affect whether the Expectation evaluates to True or False.
+    success_keys = ("min_value", "strict_min", "max_value", "strict_max")
+
+    # This dictionary contains default values for any parameters that should have default values.
+    default_kwarg_values = {}
+
+    def validate_configuration(
+        self, configuration: Optional[ExpectationConfiguration]
+    ) -> None:
+        """
+        Validates that a configuration has been set, and sets a configuration if it has yet to be set. Ensures that
+        necessary configuration arguments have been provided for the validation of the expectation.
+        Args:
+            configuration (OPTIONAL[ExpectationConfiguration]): \
+                An optional Expectation Configuration entry that will be used to configure the expectation
+        Returns:
+            None. Raises InvalidExpectationConfigurationError if the config is not validated successfully
+        """
+
+        super().validate_configuration(configuration)
+        configuration = configuration or self.configuration
+
+        # # Check other things in configuration.kwargs and raise Exceptions if needed
+        # try:
+        #     assert (
+        #         ...
+        #     ), "message"
+        #     assert (
+        #         ...
+        #     ), "message"
+        # except AssertionError as e:
+        #     raise InvalidExpectationConfigurationError(str(e))
+
+    # This method performs a validation of your metrics against your success keys, returning a dict indicating the success or failure of the Expectation.
+    def _validate(
+        self,
+        configuration: ExpectationConfiguration,
+        metrics: Dict,
+        runtime_configuration: dict = None,
+        execution_engine: ExecutionEngine = None,
+    ):
+        """Validates the given data against the set minimum and maximum value threshold for the column max """
+        column_max = metrics["column.custom_max"]
+        
+        # Obtaining components needed for validation 
+        """
+        Here just you create configurations for an already defined expectation inside of the great expectations library, 
+        this is mimicing that approach. 
+        """
+        min_value = self.get_success_kwargs(configuration).get("min_value")
+        strict_min = self.get_success_kwargs(configuration).get("strict_min")
+        max_value = self.get_success_kwargs(configuration).get("max_value")
+        strict_max = self.get_success_kwargs(configuration).get("strict_max")
+
+        """
+        We defined the range of values that should fall in the range of our strict min and max values in each column in our example. 
+        For column x we want the all values to be greater than 1,000,000 and less than 10,000,000 and that is true for this column. In 
+        contrast we see the opposite for column y. 
+        """
+        if min_value is not None: 
+            if strict_min: 
+                above_min = column_max > min_value
+            else: 
+                above_min = column_max >= min_value
+        else: 
+            above_min = True
+
+        if max_value is not None: 
+            if strict_max: 
+                below_max = column_max < max_value
+            else: 
+                below_max = column_max <= max_value
+        else: 
+            below_max = True
+
+        success = above_min and below_max
+
+        return {"success": success, "result": {"observed_value": column_max}}
+
+    # This object contains metadata for display in the public Gallery
+    library_metadata = {
+        "tags": [],  # Tags for this Expectation in the Gallery
+        "contributors": [  # Github handles for all contributors to this Expectation.
+            "@your_name_here",  # Don't forget to add your github handle here!
+        ],
+    }
+
+if __name__ == "__main__":
+    ExpectColumnMaxToBeBetweenCustom().print_diagnostic_checklist()
